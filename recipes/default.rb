3000c88fede4e97ee92a9662bb50a5d3e59a30df
venkat.naidu <venkat.naidu@e-hps.com>
2014-06-10 10:39:27 -0400

    added install script

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,7 +2,18 @@
 # Cookbook Name:: DotNetMultiTargeting
 # Recipe:: default
 #
-# Copyright 2014, YOUR_COMPANY_NAME
-#
-# All rights reserved - Do Not Redistribute
-#
+
+cookbook_file "netfx_dtp.cab" do
+	path "c:\\"
+	action :crate_if_missing
+end
+
+cookbook_file "netfx_dtp.msi" do
+	path "c:\\"
+	action :crate_if_missing
+end
+
+execute "install_it" do
+	command "NetFx_DTP.msi EXTUI=1 /log \"netfx_dtp.txt\""
+	not_if { ::File.exists?("c:\\netfx_dtp.txt")}
+end
