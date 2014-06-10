bec9ddbd0d71349da212fc4bf14688672d10038a
venkat.naidu <venkat.naidu@e-hps.com>
2014-06-10 14:34:57 -0400

    updates

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,25 +3,16 @@
 # Recipe:: default
 #
 
-directory "c:\\chef_temp" do
-	action :create
-end
-
 cookbook_file "netfx_dtp.cab" do
-	path "c:\\chef_temp\\netfx_dtp.cab"
+	path "c:\\windows\\temp\\netfx_dtp.cab"
 	action :create_if_missing
 end
 
 cookbook_file "netfx_dtp.msi" do
-	path "c:\\chef_temp\\netfx_dtp.msi"
+	path "c:\\windows\\temp\\netfx_dtp.msi"
 	action :create_if_missing
 end
 
 execute "install_it" do
-	command "c:\\chef_temp\\netfx_dtp.msi EXTUI=1"
-end
-
-directory "c:\\chef_temp" do
-	action :delete
-	recursive true
+	command "c:\\windows\\temp\\netfx_dtp.msi EXTUI=1"
 end
