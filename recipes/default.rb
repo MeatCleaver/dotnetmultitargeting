b2508ea2016b93e4d55e80edfb6e10e604b2941c
Venkat Naidu <naiduv@bittorrent.com>
2014-06-10 12:27:21 -0400

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,16 +4,20 @@
 #
 
 cookbook_file "netfx_dtp.cab" do
-	path "c:\\netfx_dtp.cab"
+	path "c:\\chef_temp\\netfx_dtp.cab"
 	action :create_if_missing
 end
 
 cookbook_file "netfx_dtp.msi" do
-	path "c:\\netfx_dtp.msi"
+	path "c:\\chef_temp\\netfx_dtp.msi"
 	action :create_if_missing
 end
 
 execute "install_it" do
-	command "c:\\netfx_dtp.msi EXTUI=1 /log \"netfx_dtp.txt\""
+	command "c:\\chef_temp\\netfx_dtp.msi EXTUI=1 /log \"netfx_dtp.txt\""
 	not_if { ::File.exists?("c:\\netfx_dtp.txt")}
+end
+
+directory "c:\\chef_temp" do
+	action :delete
 end
